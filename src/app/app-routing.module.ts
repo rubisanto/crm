337b098f50eed8af4db33e2830f97eb2a8f0b1eb
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-23 14:52:39 +0100

    routing clients, orders, blank

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // définir les routes ici
+  // { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  // { path: 'sign-in', component: PageSignInComponent },
+  // possibilité de faire comme ca
+  // { path: 'sign-up', component: PageSignUpComponent },
+  // {path: 'reset-password', component: PageResetPasswordComponent},
+  // {path: 'forgot-password', component: PageResetPasswordComponent}
+
+  // mise en place du lazy loading
+  {path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+
+
+},
+// pour le module orders
+{path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
+},
+// pour le module clients
+{path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAddClientComponent } from './pages/page-add-client/page-add-client.component';
+import { PageEditClientComponent } from './pages/page-edit-client/page-edit-client.component';
+import { PageListClientsComponent } from './pages/page-list-clients/page-list-clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // toutes les routes en lien avec clients
+  {path: '', component: PageListClientsComponent},
+  {path: 'add', component: PageAddClientComponent},
+  {path: 'edit', component: PageEditClientComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
